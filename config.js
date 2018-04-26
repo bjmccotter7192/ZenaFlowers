e81d6674fb922fe931e33f3519b2856679d7ca01
William McCotter <bjmccotter7192@gmail.com>
2018-04-26 00:41:44 -0500

    pushing up config fix

---
+++ config.js
@@ -0,0 +1,8 @@
+var SMTP_CONFIG = {
+    SMTP_USERNAME: process.env.SMTP_USERNAME,
+    SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_HOST: process.env.SMTP_HOST
+};
+
+module.exports = SMTP_CONFIG;
